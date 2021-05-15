7fdf93b35e2c863f795f4307bdc5adb8bc200774
[Henghao Li s3798993] <s3798993@student.rmit.edu.au>
2021-05-16 00:11:29 +1000

    add Student.h

---
+++ Assignment2/starter_code/Student.h
@@ -0,0 +1,20 @@
+#ifndef ASSIGN2_STUDENT_H
+#define ASSIGN2_STUDENT_H
+
+#include <string>
+using namespace std;
+
+class Student {
+    
+public:
+
+   Student();
+   ~Student();
+   
+   string name;
+   string sudentId; 
+   string email;
+
+};
+
+#endif // ASSIGN2_STUDENT_H 
